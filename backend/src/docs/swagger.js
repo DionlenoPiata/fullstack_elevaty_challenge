e2c7af555193f48adaedc3f582487b6b00826d6e
DionlenoPiata <contato.dionleno@gmail.com>
2023-12-16 17:02:32 -0300

    Updating documentation

--- backend/src/docs/swagger.js
+++ backend/src/docs/swagger.js
@@ -1,7 +1,10 @@
 const swaggerAutogen = require("swagger-autogen")({ openapi: "3.0.0" });
 
-const outputFile = "./src/docs/swagger_output.json";
-const endpointsFiles = ["./src/routes/index-route.js"];
+const outputFile = "../src/docs/swagger_output.json";
+const endpointsFiles = [
+  "../src/routes/index-route.js",
+  "../src/routes/customer-route.js",
+];
 
 const doc = {
   info: {
@@ -19,6 +22,30 @@ const doc = {
     schemas: {
       indexResponse: {
         message: `${process.env.APP_NAME}`,
+      },
+      customerResponse: {
+        fullName: "João Silva",
+        email: "joao.silva@example.com",
+        birthDate: "1990-01-15",
+        phone: "+55(11)98765-4321",
+        address: "Rua Exemplo, 123",
+        creditCards: [
+          {
+            cardNumber: "1234-5678-9012-3456",
+            cardHolder: "João Silva",
+            expirationDate: "12/23",
+            cvv: "123",
+          },
+          {
+            cardNumber: "9876-5432-1098-7654",
+            cardHolder: "Joana Silva",
+            expirationDate: "10/24",
+            cvv: "456",
+          },
+        ],
+        invoiceLink: "https://example.com/invoice",
+        createdAt: "2023-01-01T12:00:00Z",
+        updatedAt: "2023-12-14T15:30:00Z",
       },
     },
   },

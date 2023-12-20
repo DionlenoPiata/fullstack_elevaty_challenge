1af94106c97471803e626faf5d2f8881988fb298
DionlenoPiata <contato.dionleno@gmail.com>
2023-12-20 15:32:00 -0300

    Adding home page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,22 @@
-function Home() {
-  return <div>Home</div>;
-}
+// Home.js
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à página inicial!</h1>
+      <Button
+        component={Link}
+        to="/customers"
+        variant="contained"
+        color="primary"
+      >
+        Ir para a página de clientes
+      </Button>
+    </div>
+  );
+};
 
 export default Home;
